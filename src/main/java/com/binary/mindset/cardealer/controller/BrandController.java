13f8716d56aa9eec6136ab7e395e224f9f032c77
ivan_perez <ivan.pf.89@gmail.com>
2022-02-23 21:37:29 +0100

    first commit

---
+++ src/main/java/com/binary/mindset/cardealer/controller/BrandController.java
@@ -0,0 +1,63 @@
+package com.binary.mindset.cardealer.controller;
+
+import com.binary.mindset.cardealer.BrandsApi;
+import com.binary.mindset.cardealer.mapper.BrandMapper;
+import com.binary.mindset.cardealer.model.BrandDto;
+import com.binary.mindset.cardealer.model.BrandRequest;
+import com.binary.mindset.cardealer.model.BrandResponse;
+import com.binary.mindset.cardealer.service.BrandService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriBuilder;
+import javax.ws.rs.core.UriInfo;
+import java.net.URI;
+import java.util.Collections;
+import java.util.List;
+
+@RestController
+public class BrandController implements BrandsApi {
+
+    private final BrandService brandService;
+    private final BrandMapper brandMapper;
+
+    @Autowired
+    public BrandController(final BrandService brandService,
+                           final BrandMapper brandMapper) {
+        this.brandService = brandService;
+        this.brandMapper = brandMapper;
+    }
+
+    @Override
+    public Response createBrand(BrandRequest brandRequest) {
+        BrandDto brandDto = brandService.createBrand(brandMapper.toBrandDto(brandRequest));
+        return Response.created(UriBuilder.fromPath("/brands/" + brandDto.getId()).build())
+                .entity(brandMapper.toBrandResponse(brandDto))
+                .build();
+    }
+
+    @Override
+    public Response deleteBrand(Integer brandId) {
+        brandService.deleteBrand(brandId);
+        return Response.noContent().build();
+    }
+
+    @Override
+    public Response getBrand(Integer brandId) {
+        return Response.ok()
+                .entity(brandMapper.toBrandResponse(brandService.getBrand(brandId)))
+                .build();
+    }
+
+    @Override
+    public Response getBrands() {
+        List<BrandDto> brandDtos = brandService.getBrands();
+        return Response.ok()
+                .entity(brandMapper.toBrandResponseAsList(brandDtos))
+                .build();
+    }
+}
